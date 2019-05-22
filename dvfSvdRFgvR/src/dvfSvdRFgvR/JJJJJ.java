4542fd1fcce3f2df3403efa4e4d844b8aaa69794
PengjuCao <18737008789@163.com>
2019-05-22 15:07:05 +0800

    cao

--- dvfSvdRFgvR/src/dvfSvdRFgvR/JJJJJ.java
+++ dvfSvdRFgvR/src/dvfSvdRFgvR/JJJJJ.java
@@ -4,12 +4,7 @@ public class JJJJJ {
 private void SYSO() {
 	//HAFH
 	///fijiafjidfjarjfia
-//i就覅急哦IF奇偶安抚奇偶减肥
-<<<<<<< HEAD
-	//fjskjfkjfssfjasfk+sfji	
-=======
-//sfji
+	System.out.println("caopengju");
 
->>>>>>> branch 'master' of https://github.com/PengjuCao/qqqqqqqq.git
 }
 }
